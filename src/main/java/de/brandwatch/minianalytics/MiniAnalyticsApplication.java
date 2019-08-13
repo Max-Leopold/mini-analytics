0dae9aa14676c77898e1d5aa9966947c6d2bb70a
Maximilian Leopold <mleopold@invaderzim.btn1.brandwatch.net>
2019-08-13 17:56:25 +0200

    Initial commit

---
+++ src/main/java/de/brandwatch/minianalytics/MiniAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package de.brandwatch.minianalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniAnalyticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniAnalyticsApplication.class, args);
+    }
+
+}
