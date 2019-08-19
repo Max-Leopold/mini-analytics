432f48bae173861d338f8ea84202dd8a1ef2335b
Maximilian Leopold <mleopold@invaderzim.btn1.brandwatch.net>
2019-08-19 14:00:02 +0200

    Mention Storer now connects to a (seperatly started) solr database on port 8983

--- mention-storer/src/main/java/de/brandwatch/minianalytics/mentionstorer/MentionStorerApplication.java
+++ mention-storer/src/main/java/de/brandwatch/minianalytics/mentionstorer/MentionStorerApplication.java
@@ -1,13 +1,17 @@
 package de.brandwatch.minianalytics.mentionstorer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class MentionStorerApplication {
 
+    Logger logger = LoggerFactory.getLogger(MentionStorerApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(MentionStorerApplication.class, args);
     }
-
 }
--- mention-storer/src/main/java/de/brandwatch/minianalytics/mentionstorer/solr/repository/MentionRepository.java
+++ mention-storer/src/main/java/de/brandwatch/minianalytics/mentionstorer/solr/repository/MentionRepository.java
@@ -1,12 +1,14 @@
 package de.brandwatch.minianalytics.mentionstorer.solr.repository;
 
 import de.brandwatch.minianalytics.mentionstorer.model.Mention;
+import org.springframework.data.solr.repository.Query;
 import org.springframework.data.solr.repository.SolrCrudRepository;
 
 import java.util.List;
 
 public interface MentionRepository extends SolrCrudRepository<Mention, String> {
 
-    public List<Mention> findByAuthor(String author);
+    @Query(value = "*:*")
+    List<Mention> getMentions();
 
 }
