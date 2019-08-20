442fbd371f2778f78ef930ac656aeca66a2a1820
Maximilian Leopold <mleopold@invaderzim.btn1.brandwatch.net>
2019-08-20 10:01:31 +0200

    Add Kafka Consumer for mentions-generator. Reads from topic "twitter" on port 9192

--- mentions-generator/src/main/java/de/brandwatch/minianalytics/mentiongenerator/kafka/config/ProducerConfig.java
+++ mentions-generator/src/main/java/de/brandwatch/minianalytics/mentiongenerator/kafka/config/ProducerConfig.java
@@ -16,7 +16,7 @@ import java.util.Map;
 @Configuration
 public class ProducerConfig {
 
-    @Value("${kafka.bootstrap-servers}")
+    @Value("${kafka.bootstrap-servers.producer}")
     private String bootstrapServers;
 
     @Bean
