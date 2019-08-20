1f9e7d367ffa2ac1edbb3efc012ddfaf280f7c25
Max Leopold <max.leopold2111@gmail.com>
2019-08-20 15:02:48 +0200

    Add TweetFetcher Spring Service. Returns a Stream of Random Tweets.

---
+++ twitterpuller/src/main/java/de/brandwatch/minianalytics/twitterpuller/twitter/TweetFetcher.java
@@ -0,0 +1,42 @@
+package de.brandwatch.minianalytics.twitterpuller.twitter;
+
+import org.springframework.stereotype.Service;
+import twitter4j.*;
+import twitter4j.conf.ConfigurationBuilder;
+
+@Service
+public class TweetFetcher {
+
+    public TwitterStream getTwitterStream() {
+
+
+
+        TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
+        twitterStream.addListener(new StatusListener() {
+            public void onStatus(Status status) {
+                System.out.println(status.getText());
+            }
+
+            public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+            }
+
+            public void onTrackLimitationNotice(int i) {
+
+            }
+
+            public void onScrubGeo(long l, long l1) {
+
+            }
+
+            public void onStallWarning(StallWarning stallWarning) {
+
+            }
+
+            public void onException(Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        return twitterStream;
+    }
+}
