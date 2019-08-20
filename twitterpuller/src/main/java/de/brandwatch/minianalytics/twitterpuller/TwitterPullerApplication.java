3aa6de7b26f3da43ec91e216c2559f5504da81f5
Maximilian Leopold <mleopold@invaderzim.btn1.brandwatch.net>
2019-08-20 09:50:07 +0200

    Add Kafka Producer for Twitter Puller. Port:9192

---
+++ twitterpuller/src/main/java/de/brandwatch/minianalytics/twitterpuller/TwitterPullerApplication.java
@@ -0,0 +1,12 @@
+package de.brandwatch.minianalytics.twitterpuller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterPullerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterPullerApplication.class, args);
+    }
+}
