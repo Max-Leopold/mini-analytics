6b384aae4a1a5ab352868ac3da7ad9b58220f8e6
Max Leopold <max.leopold2111@gmail.com>
2019-08-23 09:08:55 +0200

    Add Ressource Model. Tweets are transformed into Ressources.

---
+++ twitterpuller/src/main/java/de/brandwatch/minianalytics/twitterpuller/model/Ressource.java
@@ -0,0 +1,35 @@
+package de.brandwatch.minianalytics.twitterpuller.model;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+public class Ressource {
+
+    private String author;
+    private String text;
+    private Instant date;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Instant getDate() {
+        return date;
+    }
+
+    public void setDate(Instant date) {
+        this.date = date;
+    }
+}
