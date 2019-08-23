076376ed22ed061319a7995f6842cdcff9833839
Max Leopold <max.leopold2111@gmail.com>
2019-08-23 09:09:21 +0200

    Create Ressources out of incoming Tweets

--- twitterpuller/src/main/java/de/brandwatch/minianalytics/twitterpuller/kafka/config/ProducerConfig.java
+++ twitterpuller/src/main/java/de/brandwatch/minianalytics/twitterpuller/kafka/config/ProducerConfig.java
@@ -1,6 +1,7 @@
 package de.brandwatch.minianalytics.twitterpuller.kafka.config;
 
 
+import de.brandwatch.minianalytics.twitterpuller.kafka.Producer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -8,13 +9,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
-@Component
 public class ProducerConfig {
 
     @Value("${kafka.bootstrap-servers}")
@@ -41,5 +40,10 @@ public class ProducerConfig {
     @Bean
     public KafkaTemplate<String, String> kafkaTemplate(){
         return new KafkaTemplate<String, String>(producerFactory());
+    }
+
+    @Bean
+    public Producer producer(){
+        return new Producer();
     }
 }
