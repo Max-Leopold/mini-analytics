d9c8e694fb5c6bc8ca2057b218c3dc3ca22ba8fd
Max Leopold <max.leopold2111@gmail.com>
2019-08-23 12:27:32 +0200

    Mention storer now consumes mentions and saves them in solr db

--- mention-storer/src/main/java/de/brandwatch/minianalytics/mentionstorer/kafka/Consumer.java
+++ mention-storer/src/main/java/de/brandwatch/minianalytics/mentionstorer/kafka/Consumer.java
@@ -1,15 +1,24 @@
 package de.brandwatch.minianalytics.mentionstorer.kafka;
 
+import de.brandwatch.minianalytics.mentionstorer.model.Mention;
+import de.brandwatch.minianalytics.mentionstorer.solr.repository.MentionRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 
 public class Consumer {
 
     private static final Logger logger = LoggerFactory.getLogger(Consumer.class);
 
+    @Autowired
+    MentionRepository mentionRepository;
+
     @KafkaListener(topics = "mentions")
-    public void receive(String message) {
-        logger.info("received message='{}'", message);
+    public void receive(Mention mention) {
+        logger.info("received message='{}'", mention);
+
+        //Save Mention in Solr db
+        mentionRepository.save(mention);
     }
 }
--- mention-storer/src/main/java/de/brandwatch/minianalytics/mentionstorer/kafka/config/ConsumerConfig.java
+++ mention-storer/src/main/java/de/brandwatch/minianalytics/mentionstorer/kafka/config/ConsumerConfig.java
@@ -2,6 +2,7 @@ package de.brandwatch.minianalytics.mentionstorer.kafka.config;
 
 
 import de.brandwatch.minianalytics.mentionstorer.kafka.Consumer;
+import de.brandwatch.minianalytics.mentionstorer.model.Mention;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -9,9 +10,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -29,7 +30,7 @@ public class ConsumerConfig {
 
         props.put(org.apache.kafka.clients.consumer.ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(org.apache.kafka.clients.consumer.ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(org.apache.kafka.clients.consumer.ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(org.apache.kafka.clients.consumer.ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
 
         props.put(org.apache.kafka.clients.consumer.ConsumerConfig.GROUP_ID_CONFIG, "mentions-storer");
         props.put(org.apache.kafka.clients.consumer.ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
@@ -38,13 +39,16 @@ public class ConsumerConfig {
     }
 
     @Bean
-    public ConsumerFactory<String, String> consumerFactory() {
-        return new DefaultKafkaConsumerFactory<String, String>(consumerConfigs());
+    public DefaultKafkaConsumerFactory consumerFactory() {
+        JsonDeserializer jsonDeserializer = new JsonDeserializer<>(Mention.class, false);
+        jsonDeserializer.addTrustedPackages("*");
+
+        return new DefaultKafkaConsumerFactory<>(consumerConfigs(), new StringDeserializer(), jsonDeserializer);
     }
 
     @Bean
-    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, String>> kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<String, String>();
+    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, Mention>> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, Mention> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
         return factory;
     }
