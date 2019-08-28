f7b46f8be0fde3d2b950ce26914415fa33a173bf
Max Leopold <max.leopold2111@gmail.com>
2019-08-28 12:35:56 +0200

    Renamed Query-input to API

--- API/src/main/java/de/brandwatch/minianalytics/queryinput/controller/Controller.java
+++ API/src/main/java/de/brandwatch/minianalytics/queryinput/controller/Controller.java
@@ -4,7 +4,6 @@ package de.brandwatch.minianalytics.queryinput.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @org.springframework.stereotype.Controller
 public class Controller {
