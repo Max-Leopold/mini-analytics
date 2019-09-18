47bb63ac23498e657f46f3404e2f4464fc45e817
Max Leopold <max.leopold2111@gmail.com>
2019-09-18 14:35:51 +0200

    Add new filter module to filter duplicate resources

---
+++ resource-filter/src/main/java/de/brandwatch/minianalytics/resourcefilter/kafka/config/ProducerConfig.java
@@ -0,0 +1,57 @@
+package de.brandwatch.minianalytics.resourcefilter.kafka.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import de.brandwatch.minianalytics.resourcefilter.kafka.Producer;
+import de.brandwatch.minianalytics.resourcefilter.model.Resource;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class ProducerConfig {
+
+    @Value("${kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Bean
+    public Map<String, Object> producerConfigs(){
+
+        Map<String, Object> producerConfig = new HashMap<>();
+
+        producerConfig.put(org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        producerConfig.put(org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        producerConfig.put(org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        return producerConfig;
+    }
+
+    @Bean
+    public ProducerFactory<String, Resource> producerFactory(){
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        JsonSerializer<Resource> jsonSerializer = new JsonSerializer<>(mapper);
+
+        return new DefaultKafkaProducerFactory<>(producerConfigs(), new StringSerializer(), jsonSerializer);
+    }
+
+    @Bean
+    public KafkaTemplate<String, Resource> kafkaTemplate(){
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    public Producer producer(){
+        return new Producer();
+    }
+}
---
+++ resource-filter/src/main/java/de/brandwatch/minianalytics/resourcefilter/model/Resource.java
@@ -0,0 +1,83 @@
+package de.brandwatch.minianalytics.resourcefilter.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.Instant;
+import java.util.Objects;
+
+public class Resource {
+
+    private String author;
+    private String text;
+    private String URL;
+    private String sourceTag;
+
+    @JsonFormat
+    private Instant date;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Instant getDate() {
+        return date;
+    }
+
+    public void setDate(Instant date) {
+        this.date = date;
+    }
+
+    public String getURL() {
+        return URL;
+    }
+
+    public void setURL(String URL) {
+        this.URL = URL;
+    }
+
+    public String getSourceTag() {
+        return sourceTag;
+    }
+
+    public void setSourceTag(String sourceTag) {
+        this.sourceTag = sourceTag;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Resource)) return false;
+        Resource resource = (Resource) o;
+        return Objects.equals(getAuthor(), resource.getAuthor()) &&
+                Objects.equals(getText(), resource.getText()) &&
+                Objects.equals(getDate(), resource.getDate());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getAuthor(), getText(), getDate());
+    }
+
+    @Override
+    public String toString() {
+        return "Resource{" +
+                "author='" + author + '\'' +
+                ", text='" + text + '\'' +
+                ", URL='" + URL + '\'' +
+                ", sourceTag='" + sourceTag + '\'' +
+                ", date=" + date +
+                '}';
+    }
+}
