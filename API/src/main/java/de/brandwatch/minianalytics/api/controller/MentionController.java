72827a7636edc2bd34417e711a846c39080bb41d
Max Leopold <max.leopold2111@gmail.com>
2019-09-18 11:40:03 +0200

    Add defaults for date and add date validator to controller

--- API/src/main/java/de/brandwatch/minianalytics/api/controller/MentionController.java
+++ API/src/main/java/de/brandwatch/minianalytics/api/controller/MentionController.java
@@ -6,6 +6,7 @@ import de.brandwatch.minianalytics.api.service.MentionService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,10 +15,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
 @RestController
 public class MentionController {
 
-    private static final Logger logger = LoggerFactory.getLogger(RestController.class);
+    private static final Logger logger = LoggerFactory.getLogger(MentionController.class);
 
     private static final Gson gson = new Gson();
 
@@ -26,17 +32,33 @@ public class MentionController {
     @Autowired
     public MentionController(MentionService mentionService) {
         this.mentionService = mentionService;
+        LocalDate.now().minusDays(1).toString();
     }
 
     @GetMapping(value = "/mentions/{queryID}")
     public ResponseEntity findMentionsFromQuery(
             @PathVariable String queryID,
-            @RequestParam(value = "startDate", defaultValue = "") String startDate,
-            @RequestParam(value = "endDate", defaultValue = "") String endDate) {
+            @RequestParam(value = "startDate", defaultValue = "")
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startDate,
+            @RequestParam(value = "endDate", defaultValue = "")
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime endDate) {
         try{
-            return ResponseEntity.status(200).body(gson.toJson(mentionService.getMentionsFromQueryID(queryID, startDate, endDate)));
+            if(startDate == null) startDate = LocalDateTime.now().minusDays(1);
+            if(endDate == null) endDate = LocalDateTime.now();
+
+            if(!validateDates(startDate, endDate)){
+                throw new DateTimeException("Dates are not in the right order");
+            }
+
+            return ResponseEntity.status(200).body(gson.toJson(mentionService.getMentionsFromQueryID(queryID,
+                    startDate.toInstant(ZoneOffset.UTC),
+                    endDate.toInstant(ZoneOffset.UTC))));
         } catch (Exception e){
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "", e);
         }
+    }
+
+    private boolean validateDates(LocalDateTime startDate, LocalDateTime endDate) {
+        return !startDate.isAfter(endDate);
     }
 }
--- API/src/main/java/de/brandwatch/minianalytics/api/service/MentionService.java
+++ API/src/main/java/de/brandwatch/minianalytics/api/service/MentionService.java
@@ -8,9 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.time.*;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.TemporalAccessor;
+import java.time.Instant;
 import java.util.List;
 
 @Component
@@ -27,48 +25,7 @@ public class MentionService {
         this.mentionRepository = mentionRepository;
     }
 
-    public List<Mention> getMentionsFromQueryID(String queryID, String startDate, String endDate) {
-
-        StringBuilder sb = new StringBuilder();
-        sb.append("[");
-
-        if(!endDate.equals("") && !startDate.equals("")){
-            if(!validateDates(convertStringDateToInstant(startDate), convertStringDateToInstant(endDate))){
-                throw new DateTimeException("Dates are not in the right order");
-            }
-        }
-
-        if (startDate.equals("")) {
-            sb.append("*");
-        } else {
-            sb.append(convertStringDateToInstant(startDate));
-        }
-
-        sb.append(" TO ");
-
-        if (endDate.equals("")) {
-            sb.append("*");
-        } else {
-            sb.append(convertStringDateToInstant(endDate));
-        }
-
-        sb.append("]");
-
-        return mentionRepository.findMentionsAfterDate(Long.parseLong(queryID), sb.toString());
-    }
-
-    private Instant convertStringDateToInstant(String date) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        String convertedDate = date + " 00:00:00";
-        TemporalAccessor temporalAccessor = formatter.parse(convertedDate);
-        LocalDateTime localDateTime = LocalDateTime.from(temporalAccessor);
-        ZonedDateTime zonedDateTime = ZonedDateTime.of(localDateTime, ZoneId.of("UTC"));
-
-        return Instant.from(zonedDateTime);
-    }
-
-
-    private boolean validateDates(Instant startDate, Instant endDate) {
-        return !startDate.isAfter(endDate);
+    public List<Mention> getMentionsFromQueryID(String queryID, Instant startDate, Instant endDate) {
+        return mentionRepository.findMentionsAfterDate(Long.parseLong(queryID), "[" + startDate + " TO " + endDate + "]");
     }
 }
