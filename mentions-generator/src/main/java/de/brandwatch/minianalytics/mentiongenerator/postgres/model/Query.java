636bd0ffa3b2adbdad929cd83ee4b1e09f565615
Max Leopold <max.leopold2111@gmail.com>
2019-10-28 10:20:59 +0100

    Synchronize Query Classes

--- mentions-generator/src/main/java/de/brandwatch/minianalytics/mentiongenerator/postgres/model/Query.java
+++ mentions-generator/src/main/java/de/brandwatch/minianalytics/mentiongenerator/postgres/model/Query.java
@@ -1,6 +1,15 @@
 package de.brandwatch.minianalytics.mentiongenerator.postgres.model;
 
-import javax.persistence.*;
+import com.google.gson.annotations.Expose;
+import de.brandwatch.minianalytics.api.security.model.User;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "queries")
@@ -9,13 +18,19 @@ public class Query {
     @Id
     @GeneratedValue
     @Column(name = "queryID")
+    @Expose
     private long queryID;
 
     @Column(name = "query")
+    @Expose
     private String query;
 
-    public Query(String query) {
+    @ManyToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    private User user;
+
+    public Query(String query, User user) {
         this.query = query;
+        this.user = user;
     }
 
     public Query() {
@@ -37,8 +52,20 @@ public class Query {
         this.query = query;
     }
 
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
     @Override
     public String toString() {
-        return query;
+        return "{" +
+                "\n\tqueryID: " + queryID +
+                "\n\tquery: " + query +
+                "\n\tuser: " + user +
+                "\n}";
     }
 }
--- twitterpuller/src/main/java/de/brandwatch/minianalytics/twitterpuller/postgres/model/Query.java
+++ twitterpuller/src/main/java/de/brandwatch/minianalytics/twitterpuller/postgres/model/Query.java
@@ -1,6 +1,16 @@
 package de.brandwatch.minianalytics.twitterpuller.postgres.model;
 
-import javax.persistence.*;
+import com.google.gson.annotations.Expose;
+import de.brandwatch.minianalytics.api.security.model.User;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 
 @Entity
 @Table(name = "queries")
@@ -9,27 +19,54 @@ public class Query {
     @Id
     @GeneratedValue
     @Column(name = "queryID")
+    @Expose
     private long queryID;
 
     @Column(name = "query")
+    @Expose
     private String query;
 
-    public Query(String query) {
+    @ManyToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    private User user;
+
+    public Query(String query, User user) {
         this.query = query;
+        this.user = user;
     }
 
     public Query() {
+    }
+
+    public long getQueryID() {
+        return queryID;
+    }
+
+    public void setQueryID(long queryID) {
+        this.queryID = queryID;
     }
 
     public String getQuery() {
         return query;
     }
 
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
     @Override
     public String toString() {
-        return "{\n" +
-                "\tqueryID: " + queryID + "\n" +
-                "\tquery: " + query + "\n" +
-                "}";
+        return "{" +
+                "\n\tqueryID: " + queryID +
+                "\n\tquery: " + query +
+                "\n\tuser: " + user +
+                "\n}";
     }
 }
